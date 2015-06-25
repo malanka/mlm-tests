bd597dbd9cb5bf433b1953e7270b5e5a1d5edddd
Michal Vyskocil <michalvyskocil@eaton.com>
2015-06-25 15:45:31 +0200

    Problem: can't debug the malamute problem
    Solution:
     * use zsys_debug so we can match the time from consumer/malamute logs
     * make sleep interval configurable

--- test2/producer.cc
+++ test2/producer.cc
@@ -1,9 +1,24 @@
 #include "malamute.h"
 #include <string>
 
-#include <stdio.h>
+#include <cstdlib>
 #include <zsys.h>
 #include <unistd.h>
+
+static
+int
+s_stoi(const std::string& inp, int def)
+{
+    try
+    {
+        return std::stoi(::getenv("SLEEP"));
+    }
+    catch (const std::exception& e)
+    {}
+
+    return def;
+}
+
 int main (int argc, char *argv []) {
     
     const char *addr = "ipc://@/malamute";
@@ -11,31 +26,38 @@ int main (int argc, char *argv []) {
 
     if ( argc > 1 )
         agent_name = agent_name + std::string(argv[1]); 
+
     mlm_client_t *client = mlm_client_new();
-    if ( !client ) {
+
+    if ( !client )
         return 1;
-    }
+
     if ( mlm_client_connect (client, addr, 1000, agent_name.c_str()) != 0 )
     {
-        mlm_client_destroy(&client);
+        mlm_client_destroy (&client);
         return 2;
     }
 
     mlm_client_set_producer (client, "test_stream"); 
-    uint64_t count = 0;
+
+    long unsigned count = 0;
+    int delay = s_stoi(::getenv("SLEEP"), 1000000);
+
+    zsys_debug("usleep(%d)", delay);
+
     while ( ( !zsys_interrupted ) && (count <= 100000 ) )
     {
         std::string topic = "from:" + agent_name + " #" + std::to_string (count);
         zmsg_t *msg_to_send = zmsg_new();
         mlm_client_send(client, topic.c_str(), &msg_to_send);
-        sleep(1);
         count++;
         if ( count % 20 == 0 )
         {
-            printf ("count is %lu\n", count );
+            zsys_debug ("%s: count is %lu", agent_name.c_str(), count);
         }
+        usleep(delay);
     }
-    printf ("total_count = %lu\n" , count);
+    zsys_debug ("%s: total_count = %lu" , agent_name.c_str(), count);
     mlm_client_destroy(&client);
     return 0;
 }
