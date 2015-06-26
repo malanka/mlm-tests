b67200ab00dfc1a4e8a324201f8b3476cf9804b8
Alena Chernikava <alenachernikava@eaton.com>
2015-06-26 12:26:29 +0200

    Problem: consumer is disconnected from the malamute under the load Solution:     from time to time publish to I_AM_ALIVE stream a message to     gisnal to malamute that consumer is still alive

--- test2/consumer.cc
+++ test2/consumer.cc
@@ -10,6 +10,9 @@ int main (int argc, char *argv []) {
     zsys_set_logstream(stderr);
     mlm_client_verbose = 1;
     
+    bool WORKAROUND = (::getenv("WORKAROUND")?true:false);
+
+
     const char *addr = "ipc://@/malamute";
     std::string agent_name = "test-agent";
 
@@ -17,7 +20,10 @@ int main (int argc, char *argv []) {
         agent_name = agent_name + std::string(argv[1]); 
 
     zsys_set_logident(agent_name.c_str());
-
+    
+    if ( WORKAROUND )
+        zsys_debug("########## with workaround");
+        
     mlm_client_t *client = mlm_client_new();
 
     if ( !client )
@@ -30,6 +36,7 @@ int main (int argc, char *argv []) {
     }
 
     mlm_client_set_consumer (client, "test_stream", ".*"); 
+    mlm_client_set_producer (client, "i_am_alive"); 
     long unsigned count = 0;
     while ( !zsys_interrupted )
     {
@@ -39,6 +46,16 @@ int main (int argc, char *argv []) {
         count++;
         std::string topic = mlm_client_subject (client);
         zsys_debug ("########## topic = %s", topic.c_str());
+        if ( WORKAROUND )
+        {
+            if ( count % 5 == 0 )
+            {
+                std::string send_topic = "I_AM_ALIVE " + std::to_string (count);
+                zmsg_t *msg_to_send = zmsg_new();
+                mlm_client_send(client, send_topic.c_str(), &msg_to_send);
+                zsys_debug ("########## send %s", send_topic.c_str());
+            }
+        }
     }
 
     zsys_debug ("########## total_count = %lu\n" , count);
